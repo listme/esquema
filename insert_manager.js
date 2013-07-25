3adbde0b7db5c1713b43dfc7fc5bd6901679fec4
munferig <munferig@gmail.com>
2013-07-26 01:17:41 +0200

    [#1] los placeholders escapan automáticamente
    https://github.com/orlandov/node-sqlite/issues/10

--- insert_manager.js
+++ insert_manager.js
@@ -1,30 +1,14 @@
 var fs = require('fs'),
     sqlite3 = require('sqlite3');
 
-var file = __dirname + '/inserts/item.json';
-var dbPath = __dirname + '/listme.db';
-
-var db = new sqlite3.Database(dbPath);
-
-
+var file = __dirname + '/inserts/item.json',
+    dbPath = __dirname + '/listme.db',
+    db = new sqlite3.Database(dbPath);
 
 fs.readFile(file, { encoding: 'utf8' }, function (err, data) {
   if (err) { return; }
 
-
   JSON.parse(data).forEach(function (elem) {
-//    console.log("insert into item values ('" + JSON.stringify(elem) + "');");
-    var sql = "insert into item(content) values ('" + JSON.stringify(elem) + "');";
-    console.log(sql);
-    db.run(sql);
-
+    db.run('INSERT INTO item (content) VALUES (?);', JSON.stringify(elem));
   });
-
-
-});
-
-db.get('select * from item;', function(err, row){
-  if (err) { console.log(err); return; }
-
-      console.log('resultados: ' + row);
 });
